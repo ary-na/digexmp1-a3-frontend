8b05616710148715e984dc5e766af41760fc848e
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-11-14 11:26:16 +1100

    Minor updates and changes.

--- src/views/pages/welcome.js
+++ src/views/pages/welcome.js
@@ -27,7 +27,7 @@ class WelcomeView {
             <div class="row justify-content-center h-100">
                 <div class="row g-4 col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 bg-white shadow-sm rounded-1 my-auto p-5">
                     <img class="logo-size d-block mx-auto mb-4" src="/images/logo-primary.svg" alt="This is an image of the coffee on café logo.">
-                    <img class="w-100 d-block" src="/images/welcome-page.svg" alt="This is an image of the coffee on café logo.">
+                    <img class="w-100 d-block" src="/images/welcome-page.png" alt="This is an image of the coffee on café logo.">
                     <h1>Welcome ${Auth.currentUser.firstName}!</h1>
                     ${Auth.currentUser.accessLevel === 1 
                             ? html`<p>Find your <span class="fst-italic">favourite barista</span>, <span class="fst-italic">drinks</span> and <span class="fst-italic">order</span> away with coffee on.</p>`
