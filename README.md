d5a28f4d97aefd3efb933e3f45c0aaa5c53762cd
Arian N. Yamchelo <ariannajafi@icloud.com>
2023-11-14 11:21:43 +1100

    Minor updates and changes.

--- README.md
+++ README.md
@@ -5,7 +5,10 @@
 > "Curtin recognises that students who are unfamiliar with the conventions of academic writing can sometimes unintentionally plagiarise or collude on assessments. This may happen if you inadequately acknowledge resources or collaborate with other students when an assessment should be done individually. An academic integrity warning is used to assign you an educative action in these situations so you can learn from your mistakes. More serious academic breaches such as cheating are managed as misconduct."
 For more information, visit [Academic Integrity at Curtin](https://www.curtin.edu.au/students/essentials/rights/academic-integrity/)
 
-## Links
+## 1. Links
 
 - [Live website](https://endearing-manatee-fc92e7.netlify.app)
 - [Backend API](https://anyamchelo-coffeeon-backend-b2n2v.ondigitalocean.app)
+
+## 2. Screenshots
+
--- src/views/pages/welcome.js
+++ src/views/pages/welcome.js
@@ -27,10 +27,10 @@ class WelcomeView {
             <div class="row justify-content-center h-100">
                 <div class="row g-4 col-xs-12 col-sm-10 col-md-8 col-lg-6 col-xl-5 bg-white shadow-sm rounded-1 my-auto p-5">
                     <img class="logo-size d-block mx-auto mb-4" src="/images/logo-primary.svg" alt="This is an image of the coffee on café logo.">
-                    <img class="w-100 d-block" src="/images/welcome-page.png" alt="This is an image of the coffee on café logo.">
+                    <img class="w-100 d-block" src="/images/welcome-page.svg" alt="This is an image of the coffee on café logo.">
                     <h1>Welcome ${Auth.currentUser.firstName}!</h1>
                     ${Auth.currentUser.accessLevel === 1 
-                            ? html`<p>Find your <span class="fst-italic">favourite barista</span>, <span class="fst-italic">drink</span> and <span class="fst-italic">order</span> away with coffee on.</p>`
+                            ? html`<p>Find your <span class="fst-italic">favourite barista</span>, <span class="fst-italic">drinks</span> and <span class="fst-italic">order</span> away with coffee on.</p>`
                             : html`<p>List your <span class="fst-italic">special drinks</span>, <span class="fst-italic">receive</span> and <span class="fst-italic">prepare orders</span> with coffee on.</p></p>`}
                     <sl-button variant="primary" class="ms-auto col-md-4" pill @click=${() => gotoRoute('/')}>Continue</sl-button>
                 </div>
